4dfcd8de046124c54daa04d04d0f591066c9c30b
william <wholm24@student.aau.dk>
2026-03-23 09:18:33 +0100

    ADD: restructure plc_xml_service with initial setup files and test configurations

---
+++ src/plc_xml_service/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'plc_xml_service'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='william',
+    maintainer_email='wholm24@student.aau.dk',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'my_node = plc_xml_service.my_node:main'
+        ],
+    },
+)
